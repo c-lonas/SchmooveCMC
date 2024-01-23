9161e712fcf43cd8ffb5b4904188bee687204c92
envelope <chaselonas@gmail.com>
2024-01-23 18:21:26 -0500

    set up SchmooveCharacterMovementComponent

---
+++ Source/Schmoove/Private/SchmooveCharacterMovementComponent.cpp
@@ -0,0 +1,88 @@
+﻿
+
+#include "SchmooveCharacterMovementComponent.h"
+
+#include "GameFramework/Character.h"
+
+bool USchmooveCharacterMovementComponent::FSavedMove_Schmoove::CanCombineWith(const FSavedMovePtr& NewMove,	ACharacter* InCharacter, float MaxDelta) const
+{
+	FSavedMove_Schmoove* NewSchmooveMove = static_cast<FSavedMove_Schmoove*>(NewMove.Get());
+
+	if (Saved_bWantsToSprint != NewSchmooveMove->Saved_bWantsToSprint)
+	{
+		return false;
+	}
+	
+	return FSavedMove_Character::CanCombineWith(NewMove, InCharacter, MaxDelta);
+}
+
+void USchmooveCharacterMovementComponent::FSavedMove_Schmoove::Clear()
+{
+	FSavedMove_Character::Clear();
+
+	Saved_bWantsToSprint = 0;
+	
+}
+
+uint8 USchmooveCharacterMovementComponent::FSavedMove_Schmoove::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if (Saved_bWantsToSprint) Result |= FLAG_Custom_0;
+	
+	return Result;
+}
+
+void USchmooveCharacterMovementComponent::FSavedMove_Schmoove::SetMoveFor(ACharacter* C, float InDeltaTime,	FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+{
+	FSavedMove_Character::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
+
+	USchmooveCharacterMovementComponent* CharacterMovement = Cast<USchmooveCharacterMovementComponent>(C->GetCharacterMovement());
+
+	Saved_bWantsToSprint = CharacterMovement->Safe_bWantsToSprint;
+}
+
+void USchmooveCharacterMovementComponent::FSavedMove_Schmoove::PrepMoveFor(ACharacter* C)
+{
+	Super::PrepMoveFor(C);
+
+	USchmooveCharacterMovementComponent* CharacterMovement = Cast<USchmooveCharacterMovementComponent>(C->GetCharacterMovement());
+
+	CharacterMovement->Safe_bWantsToSprint = Saved_bWantsToSprint;
+}
+
+USchmooveCharacterMovementComponent::FNetworkPredictionData_Client_Schmoove::FNetworkPredictionData_Client_Schmoove(const UCharacterMovementComponent& ClientMovement)
+: Super(ClientMovement)
+{
+}
+
+FSavedMovePtr USchmooveCharacterMovementComponent::FNetworkPredictionData_Client_Schmoove::AllocateNewMove()
+{
+	return FSavedMovePtr(new FSavedMove_Schmoove());
+}
+
+FNetworkPredictionData_Client* USchmooveCharacterMovementComponent::GetPredictionData_Client() const
+{
+	check(PawnOwner != nullptr)
+
+	if (ClientPredictionData == nullptr)
+	{
+		USchmooveCharacterMovementComponent* MutableThis = const_cast<USchmooveCharacterMovementComponent*>(this);
+
+		MutableThis->ClientPredictionData = new FNetworkPredictionData_Client_Schmoove(*this);
+		MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
+		MutableThis->ClientPredictionData->NoSmoothNetUpdateDist = 140.f;
+	}
+	return ClientPredictionData;
+}
+
+void USchmooveCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+
+	Safe_bWantsToSprint = (Flags & FSavedMove_Character::FLAG_Custom_0) != 0;
+}
+
+USchmooveCharacterMovementComponent::USchmooveCharacterMovementComponent()
+{
+}
---
+++ Source/Schmoove/Public/SchmooveCharacterMovementComponent.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Schmoove.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "SchmooveCharacterMovementComponent.generated.h"
+
+UCLASS()
+
+class SCHMOOVE_API USchmooveCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+	class FSavedMove_Schmoove : public FSavedMove_Character
+	{
+		typedef FSavedMove_Character Super;
+
+		uint8 Saved_bWantsToSprint:1;
+
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+		virtual void Clear() override;
+		virtual uint8 GetCompressedFlags() const override;
+		virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
+		virtual void PrepMoveFor(ACharacter* C) override;
+	};
+
+class FNetworkPredictionData_Client_Schmoove : public FNetworkPredictionData_Client_Character
+{
+public:
+	FNetworkPredictionData_Client_Schmoove(const UCharacterMovementComponent& ClientMovement);
+
+	typedef FNetworkPredictionData_Client_Character Super;
+
+	virtual  FSavedMovePtr AllocateNewMove() override;
+};
+	
+
+	bool Safe_bWantsToSprint;
+
+public:
+	virtual FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+protected:
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+
+public:
+	USchmooveCharacterMovementComponent();
+};
